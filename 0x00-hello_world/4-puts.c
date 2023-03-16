d1d46db6370b617d7980a1a05009708f734737fe
asma boutagayout <asmaboutag@gmail.com>
2023-03-16 16:58:48 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+/**
+*main - main entry
+*Return: success means 0
+*/
 int main(void)
 {
 char str1[] = "\"Programming is like building a multilingual puzzle";
