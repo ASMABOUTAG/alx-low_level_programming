0101364bdafc6cf31ff73a21a81b6f4b88efa863
asma boutagayout <asmaboutag@gmail.com>
2023-03-17 19:02:34 -0700

    this is task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  prints all single digit numbers of base 10 starting from 0
+ * Return: success is 0, failure is one.
+ */
+int main(void)
+{
+	char n = '0';
+for (n = '0'; n <= '9'; n++)
+{
+	putchar(n);
+	putchar('\n');
+}
+return (0);
+}
