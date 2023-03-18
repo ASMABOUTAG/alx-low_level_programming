828865e396bdd1d225a289ee5cd9db2d5b15062d
asma boutagayout <asmaboutag@gmail.com>
2023-03-17 18:10:27 -0700

    this is task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,12 @@
  * main - prints the alphabet in lowercase, followed by a new line.
  * Return: success is 0, failure is one.
  */
-void main(void)
+int main(void)
 {
 	char chr = 'a';
 for (chr = 'a'; chr <= 'z'; chr++)
-{
+
 	putchar(chr);
 	putchar('\n');
-}
 return (0);
 }
