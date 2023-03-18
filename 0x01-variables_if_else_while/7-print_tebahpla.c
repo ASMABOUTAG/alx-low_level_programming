5e891b8f44ed01e3df80f2a9de48a3bc4dd9694d
asma boutagayout <asmaboutag@gmail.com>
2023-03-17 19:45:36 -0700

    this is task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printsthe lowercase alphabet in reverse.
+ * Return: success is 0, failure is one.
+ */
+int main(void)
+{
+       	char ch = 'z';
+for (ch = 'z' ; ch >= 'a'; ch--)
+{
+	putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
+
