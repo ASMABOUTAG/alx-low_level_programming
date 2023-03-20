05bec13eff42f8f673d8b2532913a982007e0b05
asma boutagayout <asmaboutag@gmail.com>
2023-03-20 14:26:50 -0700

    this is task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main -  prints the size of various types .
+ * main -  prints the size of various types.
  * Return: success is 0
 */
 int main(void)
 {
-	 #warning depends on the computer is compiled and run on.
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of a int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
