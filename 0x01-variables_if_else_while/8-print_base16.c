ad7be57520c5e584de10bfecd1623f57db15b551
asma boutagayout <asmaboutag@gmail.com>
2023-03-20 11:21:38 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints all the numbers of base 16 in lowercase
+ * Return: 0 success
+*/
+int main(void)
+{
+	int c = 0;
+	char b = 'a';
+for (c = 0 ; c <= 9 ; c++)
+{
+	putchar((c % 16) + '0');
+}
+for (b = 'a' ; b <= 'f' ; b++)
+{
+	putchar(b);
+}
+putchart('\n');
+return (0);
+}
+
