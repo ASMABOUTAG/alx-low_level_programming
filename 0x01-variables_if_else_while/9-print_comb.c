515864927eb448ac28aa8cd72f7cc403bd8b430b
asma boutagayout <asmaboutag@gmail.com>
2023-03-20 12:05:49 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,12 @@
 int main(void)
 {
 	int a = 0;
-for (a = 0 ; a <= 9 ; a++)
+for (a = 0 ; a <= 8 ; a++)
 {
 	putchar(a + '0');
 	putchar(',');
 	putchar(' ');
 }
-putchar('\n');
+putchar(9 + '0');
 return (0);
 }
