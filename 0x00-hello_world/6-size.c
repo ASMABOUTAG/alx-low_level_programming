c72d60692eaf795fdd4acdd4e5ab9457610b57dd
asma boutagayout <asmaboutag@gmail.com>
2023-03-20 14:17:42 -0700

    this is task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,11 @@
 */
 int main(void)
 {
-	char chartyp;
-	int inttyp;
-	long int linttyp;
-	long long int llinttyp;
-	float floattyp;
 	 #warning depends on the computer is compiled and run on.
-printf("Size of a char: %zu byte(s)\n", sizeof(chartyp));
-printf("Size of a int: %zu byte(s)\n", sizeof(inttyp));
-printf("Size of a long int: %zu byte(s)\n", sizeof(linttyp));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(llinttyp));
-printf("Size of a float: %zu byte(s)\n", sizeof(floattyp));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of a int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
