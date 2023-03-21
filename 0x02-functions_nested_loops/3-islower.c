ce74f35bded7cc54dfa9bc38370e9b31d8364405
asma boutagayout <asmaboutag@gmail.com>
2023-03-21 16:55:53 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character.
+ * @c: the character to be cheked
+ * Return: 0 fail, 1 success.
+*/
+int _islower(int c)
+{
+int c;
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+return (0);
+}
+
+
