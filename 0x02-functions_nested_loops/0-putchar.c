8beff013b1a2e57166aa9a7cff4695722323cda7
asma boutagayout <asmaboutag@gmail.com>
2023-03-21 13:37:34 -0700

    this is task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main - will print _putchar.
  * Return: success is 0.
 */
 int main(void)
 {
-	char putch[] = "_putchar\n";
-	write(1, putch, sizeof(putch)-1);
+_putchar('_'); 
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
 	
