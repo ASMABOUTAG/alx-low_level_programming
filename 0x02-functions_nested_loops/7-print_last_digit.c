9c4716d8c1d0123db7692a2240f7c6583c6c7bbe
asma boutagayout <asmaboutag@gmail.com>
2023-03-21 18:17:13 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+# include "main.h"
+/**
+ * print_last_digit() -  prints the last digit of a number
+ * @n: the number.
+ * Return: (c) is the last digi.
+*/
+int print_last_digit(int n)
+{
+int c;
+c = n % 10;
+return (c);
+}
