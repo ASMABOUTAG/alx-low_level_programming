c763898368205175b9419abf9403ea5ebc792cee
asma boutagayout <asmaboutag@gmail.com>
2023-03-23 13:59:25 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - prints a square.
+ * @size: the size of square.
+*/
+void print_square(int size)
+{
+int i, j;
+i = 1;
+if (n > 0)
+{
+while (i <= n)
+{
+for (j = 1; j <= size; j++)
+{
+_putchar(35);
+}
+_putchar('\n');
+i++;
+}
+}
+else if (n <= 0)
+_putchar('\n');
+}
+
