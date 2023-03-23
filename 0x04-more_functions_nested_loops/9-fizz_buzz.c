b0b7f9cc933498c313d6e488f6a2b169054f7c9c
asma boutagayout <asmaboutag@gmail.com>
2023-03-23 16:19:08 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 1 to 100 with conditions
+ * Return: 0 is success.
+*/
+int main(void)
+{
+int v;
+for (v = 1; v <= 100; v++)
+{
+if ((v % 3 == 0) && (v % 5 == 0))
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else if (v % 5 == 0)
+{
+printf("Buzz");
+printf(" ");
+}
+else if (v % 3 == 0)
+{
+printf("Fizz");
+printf(" ");
+}
+else
+{
+printf("%d", v);
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
+
