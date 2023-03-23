f4b02807e5148abc5a0a57db5b2ee37a0390215c
asma boutagayout <asmaboutag@gmail.com>
2023-03-23 16:34:23 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle,
+ * @size:  the size of the triangle
+*/
+void print_triangle(int size)
+{
+int i, j, k;
+for (i = 0; i < size; i++)
+{
+for (j = size - i; j > 1; j--)
+{
+	_putchar(32);
+}
+for (k = 0; k <= i; k++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+
