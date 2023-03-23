893ae29f2d081d462a4d6d32f18357770675ca8a
asma boutagayout <asmaboutag@gmail.com>
2023-03-23 10:49:03 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal -  draws a diagonal line on the terminal
+ * @n : the number of time we repeat the digonal.
+*/
+void print_diagonal(int n)
+{
+int i, j;
+i = 1;
+if (n > 0)
+{
+while (n > i)
+{
+for (j = 1; j < i; j++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+i++;
+}
+_putchar('\n');
+}
+else if (n <= 0)
+_putchar('\n');
+}
