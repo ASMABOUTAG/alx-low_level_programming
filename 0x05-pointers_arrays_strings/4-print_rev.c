7cffef35c74a564c7333bfce2106c5f9e932b3d3
asma boutagayout <asmaboutag@gmail.com>
2023-03-28 16:36:04 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line
+ * @s: pointer to a char.
+*/
+void print_rev(char *s)
+{
+int i, len;
+i = 0;
+while (*s != '\0')
+{
+s++;
+i++;
+}
+for (len = i; len >= 0; len--)
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
