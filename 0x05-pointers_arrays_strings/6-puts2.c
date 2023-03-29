15420689de50aa35f9c000f45d76006248183d8d
asma boutagayout <asmaboutag@gmail.com>
2023-03-28 17:55:54 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 -  prints every other character of a string...
+ *@str: pointer to char.
+*/
+void puts2(char *str)
+{
+int count = 0;
+while (*s != '\0')
+{
+if (count % 2 == 0)
+{
+_putchar(*s);
+}
+s++;
+count++;
+}
+_putchar('\n');
+}
