0fdc1401fc5421616e3decdd858ac877cf340a27
asma boutagayout <asmaboutag@gmail.com>
2023-03-30 16:21:10 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings...
+ * @dest: pointer to char.
+ * @src: pointer to char.
+ * @n: number of bytes.
+ * Return: pointer to the resulting string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+i = 0;
+j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+return (dest);
+}
