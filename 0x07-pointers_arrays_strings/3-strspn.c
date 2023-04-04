43988d54f15c204ed8cc0ba1584643f42c2acad4
asma boutagayout <asmaboutag@gmail.com>
2023-04-04 10:00:42 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: string where to search
+ * @accept: char to seearch
+ * Return: return length
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j, k;
+k = 0;
+for (i = 0; s[i] > '\0'; i++)
+{
+for (j = 0; accept[j]; j++)
+{
+if (s[i] == accept[j])
+{
+k++;
+break;
+}
+else if (aceept[j + 1] == '\0')
+return (k);
+}
+}
+return (k);
+}
