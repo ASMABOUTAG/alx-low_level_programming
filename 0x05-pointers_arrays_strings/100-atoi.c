e0d460f739f12a76a9a4c46b328cd0675ed5eaaf
asma boutagayout <asmaboutag@gmail.com>
2023-04-09 16:45:22 -0700

    task 10 #advanced

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _atoi -  convert a string to an integer.
+ * @s: a char.
+ * Return: return the integer.
+*/
+int _atoi(char *s)
+{
+int i;
+int num = 0;
+int sign = 1;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 48 && s[i] <= 57)
+num = num * 10 + (s[i] - 48);
+if (s[i] == 45)
+{
+sign = -1 * sign;
+}
+}
+num = num *sign;
+return (num);
+}
