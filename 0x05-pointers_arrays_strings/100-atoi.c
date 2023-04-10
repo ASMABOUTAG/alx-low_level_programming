bbff533ad53283f05c29a06f35adcd5a5028cdea
asma boutagayout <asmaboutag@gmail.com>
2023-04-09 17:37:03 -0700

    task 10 #advanced

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int _atoi(char *s)
 int i;
 unsigned int number = 0;
 int num_sign = 1;
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; s[i]; i++)
 {
 if (s[i] == 45)
 {
@@ -17,7 +17,7 @@ num_sign = -1 * num_sign;
 }
 if (s[i] >= 48 && s[i] <= 57)
 number = number * 10 + (s[i] - 48);
-if (number > 0)
+else if (number > 0)
 break;
 }
 number = number *num_sign;
