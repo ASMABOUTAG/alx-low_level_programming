b022ddf7700800d9fba1fbf913184d983f019e7a
asma boutagayout <asmaboutag@gmail.com>
2023-04-10 16:00:12 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line
+ * @argc: the size of argv array
+ * @argv:  an array of size argc
+ * Return: success 0.
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
