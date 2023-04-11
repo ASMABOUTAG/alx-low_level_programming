2d7c5b4ef91ff166405a7bbb9023fccb42e3bfbd
asma boutagayout <asmaboutag@gmail.com>
2023-04-11 09:31:32 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: the size of argv
+ * @argv: an array of size argc
+ * Return: 0 success, 1 other conditions.
+*/
+int main(int argc, char *argv[])
+{
+int i;
+int add = 0;
+if (argc == 1)
+{
+printf("0\n");
+return (1);
+}
+for (i = 1; i < argc; i++)
+{
+if (argc > 1)
+{
+add = add + atoi(argv[i]);
+if (atoi(argv[i]) == 0 || atoi(argv[i]) < 0)
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+printf("%d\n", add);
+return (0);
+}
