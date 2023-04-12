59120bad9249f3dfd1ec2e60d2495ee708c01fec
asma boutagayout <asmaboutag@gmail.com>
2023-04-12 15:54:24 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,14 @@ unsigned int j;
 char *p = malloc(sizeof(char) * size);
 if (size == 0)
 	return (NULL);
+if (p != NULL)
+{
 for (j = 0; j < size; j++)
 {
 	p[j] = c;
 }
 return (p);
 }
-
+else
+return (NULL);
+}
