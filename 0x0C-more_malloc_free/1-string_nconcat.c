88c94b64b238a9d23caf2abd1a57be811b60305b
asma boutagayout <asmaboutag@gmail.com>
2023-04-13 17:55:46 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,30 +9,32 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, size_1, size_2;
+unsigned int i, j, size1, size2;
 char *string;
-j = i = 0;
-size_1 = size_2 = 0;
-while (s1 && s1[size_1])
-	size_1++;
-while (s2 && s2[size_2])
-	size_2++;
-if (n > size_2)
+j = 0;
+i = 0;
+size1 = 0;
+size2 = 0;
+while (s1 && s1[size1])
+	size1++;
+while (s2 && s2[size2])
+	size2++;
+if (n > size2)
 {
-string = malloc(sizeof(char) * (size_1 + n + 1));
+string = malloc(sizeof(char) * (size1 + n + 1));
 }
 else
-string = malloc(sizeof(char) * (size_2 + size_1 + 1));
+string = malloc(sizeof(char) * (size2 + size1 + 1));
 if (!string)
 return (NULL);
-while (i < size_1)
+while (i < size1)
 {
 string[i] = s1[i];
 i++;
 }
-while (n < size_2 && i < (size_1 + n))
+while (n < size2 && i < (size1 + n))
 string[i++] = s2[j++];
-while (n >= size_2 && i < (size_1 + size_2))
+while (n >= size2 && i < (size1 + size2))
 string[i++] = s2[j++];
 string[i] = '\0';
 return (string);
