7a32f090cb5fcdc9f8f466a2a99061a24857fec4
asma boutagayout <asmaboutag@gmail.com>
2023-04-27 14:55:35 -0700

    header file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(int c);
+/**
+ * struct node - singly linked list
+ * @str: a string
+ * @l: string's length
+ * @next: to the next node.
+*/
+
+typedef struct node
+{
+char *str;
+unsigned int l;
+struct node *next;
+} list_t;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
